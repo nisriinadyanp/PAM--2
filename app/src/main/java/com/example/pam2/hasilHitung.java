f893dc26f7779a77fce2c8aa839a911892286062
nisriinadyanp <nisriinadyanp@gmail.com>
2024-09-09 22:52:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pam2/hasilHitung.java
@@ -0,0 +1,36 @@
+package com.example.pam2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+public class hasilHitung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hasil_hitung);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+
+        TextView tampilhitung = findViewById(R.id.tampilHitung);
+
+        if(intent.hasExtra( "pesanError")) {
+            tampilhitung.setText(intent.getStringExtra( "pesanError"));
+        } else {
+            tampilhitung.setText(String.valueOf(intent.getDoubleExtra("hasil", 0)));
+        }
+    }
+}
